51ce88a0a00442423945f438762a463a1cbcdfc7
LiliangW0702 <liliangw@umich.edu>
2022-04-14 22:35:05 -0400

    added a contol policy with PID+gaussian noise

--- data_collection.py
+++ data_collection.py
@@ -32,22 +32,22 @@ def keep_comment_clean(new_value, original_line): #output value&origianl comment
 
     return f"{str(new_value)}" + " " * num_of_spaces + comment
 
-def simulation(value_107,value_AP,value_w0,value_w1,value_w2,path,path_wheel,line_107,line_AP, line_w0,line_w1,line_w2,rl_file):
-    
+#def simulation(value_107,value_AP,value_w0,value_w1,value_w2,path,path_wheel,line_107,line_AP, line_w0,line_w1,line_w2,rl_file):
+def set_outputInterval(value, path, line_number):
     lines = list()
-    # change F10.7 and AP
+    # change Output Interval
     with open(path,'r') as f:
         lines = f.readlines()
 
-        index_107 = line_107 - 1
-        lines[index_107] = keep_comment_clean(str(value_107), lines[index_107])
-
-        index_AP = line_AP - 1
-        lines[index_AP] = keep_comment_clean(str(value_AP), lines[index_AP])
+        index = line_number - 1
+        lines[index] = keep_comment_clean(str(value), lines[index])
 
     with open(path, "w+") as new_f:
         for line in lines:
             new_f.write(line)
+    
+def simulation(value_w0,value_w1,value_w2,path_wheel,line_w0,line_w1,line_w2,rl_file):   
+    lines = list()
             
     # change initial momentum
     with open(path_wheel,'r') as f:
@@ -91,8 +91,8 @@ def simulation(value_107,value_AP,value_w0,value_w1,value_w2,path,path_wheel,lin
     # Print metadata for rl.42
     with open(f"{RESULT_ORIGINAL_DIR}/RL.42", "r") as result_time:
         results = result_time.readlines()
-        rl_file.write(f"Value F10.7 = {value_107}.\n")
-        rl_file.write(f"Value AP = {value_AP}.\n")
+        #rl_file.write(f"Value F10.7 = {value_107}.\n")
+        #rl_file.write(f"Value AP = {value_AP}.\n")
         rl_file.write(f"Value Wheel0 = {value_w0}.\n")
         rl_file.write(f"Value Wheel1 = {value_w1}.\n")
         rl_file.write(f"Value Wheel2 = {value_w2}.\n")
@@ -104,21 +104,18 @@ def simulation(value_107,value_AP,value_w0,value_w1,value_w2,path,path_wheel,lin
 
 
 if __name__ == "__main__":
-    N_107=1 #number of samples of F10.7
-    N_AP=1 #number of samples of AP
+    #N_107=1 #number of samples of F10.7
+    #N_AP=1 #number of samples of AP
+    Value_OutputInterval=5.0
     N_W0=2 #number of samples of momentum of axis 0
-    N_W1=1 #number of samples of momentum of axis 1
-    N_W2=1 #number of samples of momentum of axis 2
-    Low_107=100
-    High_107=230
-    Low_AP=50
-    High_AP=100
-    Low_W0=-0.05
-    High_W0=0.05
-    Low_W1=-0.01
-    High_W1=0.01
-    Low_W2=-0.01
-    High_W2=0.01
+    N_W1=2 #number of samples of momentum of axis 1
+    N_W2=2 #number of samples of momentum of axis 2
+    Low_W0=-0.15
+    High_W0=0.15
+    Low_W1=-0.03
+    High_W1=0.03
+    Low_W2=-0.03
+    High_W2=0.03
     Dist_type_107='UNIFORM'
     Dist_type_AP='UNIFORM'
     Dist_type_W0='UNIFORM'
@@ -134,24 +131,25 @@ if __name__ == "__main__":
         print(f"{RESULT_DIR} doesn't exist, creating one...")
         os.mkdir(RESULT_DIR)
     
-    Samples_107=generate_samples(N_107,Low_107,High_107,Dist_type_107)
-    Samples_AP=generate_samples(N_AP,Low_AP,High_AP,Dist_type_AP)
+    #Samples_107=generate_samples(N_107,Low_107,High_107,Dist_type_107)
+    #Samples_AP=generate_samples(N_AP,Low_AP,High_AP,Dist_type_AP)
     Samples_W0=generate_samples(N_W0,Low_W0,High_W0,Dist_type_W0)
     Samples_W1=generate_samples(N_W1,Low_W1,High_W1,Dist_type_W1)
     Samples_W2=generate_samples(N_W2,Low_W2,High_W2,Dist_type_W2)
-    Samples =np.array(list(itertools.product(Samples_107,Samples_AP,Samples_W0,Samples_W1,Samples_W2)))
+    Samples =np.array(list(itertools.product(Samples_W0,Samples_W1,Samples_W2)))
 
     #time_file = open(f"{RESULT_DIR}/time.txt", "w+")
     rl_file = open(f"{RESULT_DIR}/rl.txt", "w+")
 
     try:
+        set_outputInterval(Value_OutputInterval, Path_to_file, 5)
         for ii in range(len(Samples)):
-            Value_107=round(Samples[ii,0], 1)
-            Value_AP=round(Samples[ii,1], 1)
-            Value_W0=round(Samples[ii,2],4)
-            Value_W1=round(Samples[ii,3],4)
-            Value_W2=round(Samples[ii,4],4)
-            simulation(Value_107, Value_AP, Value_W0,Value_W1,Value_W2,Path_to_file,Path_to_file2, 19, 20, 62, 71, 80, rl_file)
+            #Value_107=round(Samples[ii,0], 1)
+            #Value_AP=round(Samples[ii,1], 1)
+            Value_W0=round(Samples[ii,0],4)
+            Value_W1=round(Samples[ii,1],4)
+            Value_W2=round(Samples[ii,2],4)
+            simulation(Value_W0,Value_W1,Value_W2,Path_to_file2, 62, 71, 80, rl_file)
     except:
         print(f"Oops, something went wrong: {e}")
     finally:
